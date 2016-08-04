f5de646a78ffd1e963d13521c861cd1deb345901
Andrew Myhre <andrew.myhre@gmail.com>
2016-08-04 18:48:13 -0400

    log unhandled errors

--- BlessTheWeb.MVC5/Global.asax.cs
+++ BlessTheWeb.MVC5/Global.asax.cs
@@ -12,6 +12,8 @@ namespace BlessTheWeb.MVC5
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        static log4net.ILog log = log4net.LogManager.GetLogger(typeof(MvcApplication));
+
         protected void Application_Start()
         {
             log4net.Config.XmlConfigurator.Configure();
@@ -22,8 +24,42 @@ namespace BlessTheWeb.MVC5
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            var log = log4net.LogManager.GetLogger(typeof(MvcApplication));
             log.Info("Application started");
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            if (exception != null)
+            {
+                log.Error("Unhandled exception", exception);
+            }
+
+            HttpException httpException = exception as HttpException;
+            if (httpException != null)
+            {
+                RouteData routeData = new RouteData();
+                routeData.Values.Add("controller", "Error");
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        // page not found
+                        //routeData.Values.Add("action", "HttpError404");
+                        break;
+                    case 500:
+                        // server error
+                        //routeData.Values.Add("action", "HttpError500");
+                        break;
+                    default:
+                        routeData.Values.Add("action", "Index");
+                        break;
+                }
+                routeData.Values.Add("error", exception.Message);
+                // clear error on server
+                Server.ClearError();
+
+                Response.RedirectToRoute(routeData.Values);
+            }
         }
     }
 }
